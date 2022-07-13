d70dbdd48b3bd3057eed9bd8b6f8af33870fe854
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-13 17:06:35 +0900

    basics #6-1

---
+++ 1. basics/ex06/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    // const App = React.createElement('div', null, 'Hello World');    
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>        
+    );
+}
+
+export {App}
---
+++ 1. basics/ex06/src/index.js
@@ -0,0 +1,7 @@
+import { ReactDOM } from 'react';
+import {App} from './App.js';   
+
+// document
+//     .getElementById('root')
+//     .appendChild(App());
+ReactDOM.render(App(), document.getElementById('root'));
