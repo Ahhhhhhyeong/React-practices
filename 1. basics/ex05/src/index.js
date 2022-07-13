e81bb9bdee9e2cd19550e80ed6621ec7494b94dc
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-13 16:53:57 +0900

    basics #05

--- 1. basics/ex05/src/index.js
+++ 1. basics/ex05/src/index.js
@@ -1,5 +1,7 @@
-import {App} from './App.js';   //의존성 생성 / APP모듈 import
+import { ReactDOM } from 'react';
+import {App} from './App.js';   
 
-document
-    .getElementById('root')
-    .appendChild(App());
+// document
+//     .getElementById('root')
+//     .appendChild(App());
+ReactDOM.render(App(), document.getElementById('root'));
