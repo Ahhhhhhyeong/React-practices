60de375751df745b18c0ce6849707351af1b8d22
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-15 14:54:06 +0900

    component validate #fin

--- 2. component/ex01/src/04/App.js
+++ 2. component/ex01/src/04/App.js
@@ -12,6 +12,13 @@ function App() {
                 props04 = {{no:1, name:'둘리'}}
                 props05 = {['Hello', 'World', 'ReactJS', 'ES6']}
                 props06 = {() => '함수' }
+                props07 = {'숫자나 글자만 들어가유'}
+                props08 = {[true, false, true]}
+                props09 = {{
+                    no:1, 
+                    name:'둘리', 
+                    email:'dooly@gmail.com'
+                }}
             />
             </div>
         );
