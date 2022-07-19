a74b46d4b5987d36373e6b60cbe3ac24c171737d
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-19 11:26:33 +0900

    event #fin

--- 2. component/ex03/src/04/App.js
+++ 2. component/ex03/src/04/App.js
@@ -2,31 +2,31 @@ import React, {Fragment, useRef} from 'react';
 import logo from '../assets/images/react-logo.png';
 
 export default function App() {
-    const imageRef = useRef(null);
-    // 이벤트 함수 만들기
-    const onKeyPressInput = (e) => { 
-        if(e.key === 'Enter'){
+    const imgRef = useRef(null);
+
+    const onKeyPressInput = e => {
+        if(e.key === 'Enter') {
             console.log(e.target.value);
         }
-     };
-    const onFocusInput = (e) => { console.log('Focus Input') };
-    const onBlurInput = (e) => { console.log('onBlurInput') };
-    
-    const onMouseOverImage = (e) => console.log('mouseover ', `x=${e.clientX}, y=${e.clientY}`);
-    const onMouseMoveImage = e => {
-        console.log('mousemove ', `x=${e.clientX}, y=${e.clientY}`);
-        
     }
-    const onMouseOutImage = e => console.log('MouseOut ', `x=${e.clientX}, y=${e.clientY}`);
-    const onMouseDownImage =e => console.log('MouseDown ', `x=${e.clientX}, y=${e.clientY}`);
-    const onMouseUpImage = e => console.log('MouseUp ', `x=${e.clientX}, y=${e.clientY}`);
+    const onFocusInput = e => console.log('focused');
+    const onBlurInput = e => console.log('blur');
 
-    const onClickImage = e => console.log('Click ');
-    const onDoubleClickImage = e => console.log('DoubleClick ');
+    const onMouseOverImage = e => console.log('mouseover', `x=${e.clientX}, y=${e.clientY}`);
+    const onMouseMoveImage = e => {
+        const offsetTop = imgRef.current.offsetTop;
+        const offsetLeft = imgRef.current.offsetLeft;
+        console.log('mousemove', `x=${e.clientX-offsetLeft}, y=${e.clientY-offsetTop}`);
+    }
+    const onMouseOutImage = e => console.log('mouseout', `x=${e.clientX}, y=${e.clientY}`);
+    const onMouseDownImage = e => console.log('mousedown', `x=${e.clientX}, y=${e.clientY}`);
+    const onMouseUpImage = e => console.log('mouseup', `x=${e.clientX}, y=${e.clientY}`);
+    const onClickImage = e => console.log('click', `x=${e.clientX}, y=${e.clientY}`);
+    const onDoubleClickImage = e => console.log('dblclick', `x=${e.clientX}, y=${e.clientY}`);
 
     return (
         <Fragment>
-            <h2>ex03 - Some Examples of Event Handler</h2>
+            <h2>Some Examples of Event Handler</h2>
             <input
                 type='text'
                 placeholder='메세지를 입력 하세요'
@@ -36,6 +36,7 @@ export default function App() {
                 <br/>
                 <br/>
             <img
+                ref = {imgRef}
                 style={ {
                     cursor: 'pointer',
                     width: 190,
