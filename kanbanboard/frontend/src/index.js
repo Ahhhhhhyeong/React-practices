3c706d6897db45c0fd1d7e7f6800e96c1d061f8f
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-20 18:00:22 +0900

    kandanboard #2

--- kanbanboard/frontend/src/index.js
+++ kanbanboard/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import KanbanBoard from './KanbanBoard';
-import './assets/css/.css';
+//import KanbanBoard from './KanbanBoard';
+import './assets/css/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
