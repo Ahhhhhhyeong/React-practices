8e9b4f118e46f534743b3ed644e7af53f660660f
Ahhhhhhyeong <dkdud081@gmail.com>
2022-07-26 10:29:20 +0900

    add ex04

---
+++ 2. component/ex05/src/04/App.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import './assets/scss/App.scss'
+import Clock from './Clock';
+
+export default function App() {
+    
+    const [currentTime, setcurrentTime] = 
+    
+    const _getCurrentClockTime = () => {
+        const now = new Date();
+        const hours = now.getHours();
+
+        return {
+            hours: ('0' + (hours > 12 ? hours - 12 : hours)).slice(-2),
+            minutes: ('0' + now.getMinutes()).slice(-2),
+            seconds: (('0')+now.getSeconds()).slice(-2),
+            session: hours > 12 ? 'pm': 'am'
+        }
+    }
+
+    useEffect(() => {
+        console.log('After Mount(componentDidMount)');
+
+ 
+        return (() => {
+            console.log('After Unmount(componentWillUnmount)');
+        });       
+    }, []);
+
+   
+    return (
+            <Clock
+                message={'ex05: useEffect Hook example'}
+                hours={hours}
+                minutes={minutes}
+                seconds={seconds}/>
+    );
+}
