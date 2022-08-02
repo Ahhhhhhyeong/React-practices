e03aa4c66ecd7e1d2029a081710a74ec204a4644
Ahhhhhhyeong <dkdud081@gmail.com>
2022-08-02 13:28:03 +0900

    routing #3

--- 3. routing/src/04/component/Main.js
+++ 3. routing/src/04/component/Main.js
@@ -5,7 +5,19 @@ export default function Main() {
     return (
         <div>
             <h1>Main</h1>
-
+            <Link>
+                {/* <ul>
+                    <li><Link to={"/"}>[Main]</Link></li>
+                    <li><Link to={"/guestbook"} >[Guestbook]</Link></li>
+                    <li><Link to={"/gallery"} >[Gallery]</Link></li>
+                </ul> */}
+                <ul>
+                    <li><NavLink to={"/"}>[Main]</NavLink></li>
+                    <li><NavLink to={"/guestbook"} >[Guestbook]</NavLink></li>
+                    <li><NavLink to={"/gallery"} >[Gallery]</NavLink></li>
+                </ul>
+                {/*  */}
+            </Link>
         </div>
     );
 }
